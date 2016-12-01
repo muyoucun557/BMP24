3089c85d48582e1e7f4638a0325c2fa00bf56ed8
muyoucun557 <1291979978@qq.com>
2016-12-01 21:24:02 +0800

    修复一些bug
    修复一些bug

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-
-var BMP24 = require("./lib/BMP24.js");
-var http = require("http");
-var fs = require("fs");
+'use strict'
+const nums = [0,1,2,3,4,5,6,7,8,9];
+const BMP24 = require("./lib/BMP24.js");
+const http = require("http");
 
 var server = http.createServer(function(req,res){
-	var bmp = new BMP24(100,100);
-    bmp.drawChar(5,0,0,(255<<16));
+    var bmp = new BMP24(100,40);
+    var str = createCode();
+    bmp.drawStr(str,10,12,(255<<16),10);
     res.setHeader("Content-Type","image/bmp");
     res.end(bmp.data);
 });
-
 server.listen(8080);
+
+//生成4位的随机验证码
+function createCode(){
+	var arrs = [];
+	for(var i=0; i<4; i++){
+		var index = Math.floor(Math.random()*10);
+		var num = nums[index];
+		arrs.push(num);
+	}
+	return arrs.join("");
+}
